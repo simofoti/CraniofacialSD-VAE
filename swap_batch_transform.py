4af717e74d4d794811facbedbd808fd3ee6637d5
Simone <simofoti@gmail.com>
2022-02-17 15:37:59 +0000

    Batch also labels when swapping features

--- swap_batch_transform.py
+++ swap_batch_transform.py
@@ -15,16 +15,18 @@ class SwapFeatures:
         new_batch = torch.zeros([batch_size ** 2, *batched_data.x.shape[1:]],
                                 device=batched_data.x.device,
                                 dtype=batched_data.x.dtype)
+        new_y = [None] * (batch_size ** 2)
         key = random.choice(self._zones_keys)
         for j in range(batch_size):
             for i in range(batch_size):
                 if i == j:
                     new_batch[i * batch_size + j, ::] = batched_data.x[i, ::]
+                    new_y[i * batch_size + j] = batched_data.y[i]
                 else:
                     vertices = batched_data.x.numpy()
                     new_batch[i * batch_size + j, ::] = self.swap(
                         vertices[i, ::], vertices[j, ::], key)
-        batched_data = Data(x=new_batch, swapped=key)
+        batched_data = Data(x=new_batch, y=new_y, swapped=key)
         return batched_data
 
     def swap(self, verts0, verts1, feature_key):
